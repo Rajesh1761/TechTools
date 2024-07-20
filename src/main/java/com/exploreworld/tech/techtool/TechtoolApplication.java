34e0a5f3906f6e217c9c88b5e5ebf8921b6e60b1
Rajesh Mishra <40909309+Rajesh1761@users.noreply.github.com>
2024-07-20 14:40:07 +0100

    Initial Upload

---
+++ src/main/java/com/exploreworld/tech/techtool/TechtoolApplication.java
@@ -0,0 +1,13 @@
+package com.exploreworld.tech.techtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exploreworld/tech/techtool/TechtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploreworld.tech.techtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
